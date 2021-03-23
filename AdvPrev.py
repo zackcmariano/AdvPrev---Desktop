77358d2e26dbf7f5227924ff0b8476b26aaeca93
Zack Mariano <zack.cmariano@gmail.com>
2021-03-23 15:21:30 -0300

    Algoritmo / Python

--- AdvPrev.py
+++ AdvPrev.py
@@ -1,9 +1,13 @@
+'''IMPORT OF LIBRARIES REQUIRED FOR 
+THE CONSTRUCTION OF ALGORITHM'''
 from PyQt5 import uic, QtWidgets
 from PyQt5 import QtGui
 import mysql.connector
 import webbrowser
 
-'''IMPORTING THE DATABASE MODULE'''
+
+#CONNECTION TO THE MYSQL DATABASE 
+#FOR VALIDATION AND INSERTING OF APPLICATION DATA.
 banco = mysql.connector.connect(
         host="127.0.0.1",
         port="3306",
@@ -12,6 +16,9 @@ banco = mysql.connector.connect(
         database="advprev"
     )
 
+
+#INITIAL APPLICATION SCREEN, RECEIVING USER DATA AND 
+# VALIDATING IN THE DATABASE
 def loginscreen():
     user = login.lineEdit.text()
     passw = login.lineEdit_2.text()
@@ -29,10 +36,14 @@ def loginscreen():
         login.lineEdit.setText("")
         login.lineEdit_2.setText("")
 
+
+#OPENING THE REGISTRATION SCREEN
 def registerscreen():
     login.close()
     register.show()
 
+
+#CONNECTING TO THE DATABASE AND INSERTING A NEW USER
 def newregister():
     login = register.lineEdit_5.text()
     nome = register.lineEdit.text()
@@ -49,40 +60,53 @@ def newregister():
             banco.close()
             register.label_3.setText("Usuario Cadastrado com Sucesso")
 
-
         except mysql as erro:
             print("Print ao inserir dados", erro)
 
     else:
         register.label_3.setText("Dados incompletos ou senhas não conferem")
 
+
+#ACCESS HOME
 def home():
     logged.frame_3.show()
 
+
+#ACCESS WEB SCREEN
 def web():
     logged.frame_2.show()
     logged.frame_3.close()
 
+
+#ACCESS DATA SCREEN
 def graph():
     logged.frame.show()
     logged.frame_2.close()
 
+
+#ACCESS THE JUSBRASIL WEBSITE
 def jusbras():
     webbrowser.open('https://www.jusbrasil.com.br/home')
 
+
+#ACCESS THE INSS WEBSITE
 def inss():
     webbrowser.open('https://meu.inss.gov.br/')
 
+
+#FINISH APPLICATION
 def close():
     logged.close()
 
 
-#READING THE UI FILES#
+#READING THE SCREEN FILES IN UI FORMAT
 app=QtWidgets.QApplication([])
 login=uic.loadUi("login.ui")
 register=uic.loadUi("register.ui")
 logged=uic.loadUi("wid_logg.ui")
 
+
+#ADDING FUNCTIONALITY TO THE SCREEN BUTTONS
 login.pushButton_2.clicked.connect(registerscreen)
 login.pushButton.clicked.connect(loginscreen)
 register.pushButton.clicked.connect(newregister)
@@ -93,17 +117,20 @@ logged.pushButton_7.clicked.connect(close)
 logged.pushButton.clicked.connect(jusbras)
 logged.pushButton_6.clicked.connect(inss)
 
-#INSERT IMAGE#
-icon=QtGui.QPixmap([])
-login.label_4.setPixmap(QtGui.QPixmap('logoadv.png'))
-login.label_5.setPixmap(QtGui.QPixmap('justice.png'))
-register.label_4.setPixmap(QtGui.QPixmap('logoadv_rg.png'))
-logged.label_17.setPixmap(QtGui.QPixmap('icon.png'))
-logged.label_23.setPixmap(QtGui.QPixmap('logoadv_rg.png'))
-logged.label_19.setPixmap(QtGui.QPixmap('prev.png'))
-logged.label_11.setPixmap(QtGui.QPixmap('pagjus.png'))
-logged.label.setPixmap(QtGui.QPixmap('GraphStates.png'))
-logged.label_2.setPixmap(QtGui.QPixmap('GraphMotive.png'))
 
+#INSERTING IMAGES ON SCREENS
+icon=QtGui.QPixmap([])
+login.label_4.setPixmap(QtGui.QPixmap('img\logoadv.png'))
+login.label_5.setPixmap(QtGui.QPixmap('img\justice.png'))
+register.label_4.setPixmap(QtGui.QPixmap('img\logoadv_rg.png'))
+logged.label_17.setPixmap(QtGui.QPixmap('img\icon.png'))
+logged.label_23.setPixmap(QtGui.QPixmap('img\logoadv_rg.png'))
+logged.label_19.setPixmap(QtGui.QPixmap('img\prev.png'))
+logged.label_11.setPixmap(QtGui.QPixmap('img\pagjus.png'))
+logged.label.setPixmap(QtGui.QPixmap('img\GraphStates.png'))
+logged.label_2.setPixmap(QtGui.QPixmap('img\GraphMotive.png'))
+
+
+#INITIALIZING THE APPLICATION
 login.show()
 app.exec()
